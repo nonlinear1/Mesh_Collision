8d61510a3bb375a3792504edb68baf2a08a0af67
meir shomron <meirshomron@gmail.com>
2018-05-02 16:50:26 +0300

    Update main.cpp

--- ClassExample2/main.cpp
+++ ClassExample2/main.cpp
@@ -35,8 +35,6 @@ bool SolveEquision(Node* A, Node* B, glm::vec3 L, glm::mat4 TRTB, glm::mat4 TRTA
 
 bool checkCollision(Node* A, Node* B, glm::mat4 TRTB, glm::mat4 TRTA) {
 
-	//std::cout << "S-----------------------S" << std::endl;
-
 	bool collison = true;
 	glm::vec3 L;
 
@@ -100,9 +98,6 @@ bool checkCollision(Node* A, Node* B, glm::mat4 TRTB, glm::mat4 TRTA) {
 	if (SolveEquision(A, B, L, TRTB, TRTA))
 		collison = false;
 
-
-	//std::cout << "E-----------------------E" << std::endl;
-
 	return collison;
 }
 
@@ -120,7 +115,6 @@ void print_queue(std::queue<std::pair<Node*, Node*>> q)
 
 
 bool findCollision( Scene& scn, int first, int second) {
-	//scn.shapes[first]->checkCollision(scn.shapes[first]->mesh->myKdtree.getRoot(), scn.shapes[second]->mesh->myKdtree.getRoot());
 	queue.push(std::pair<Node*, Node*>(scn.shapes[first]->mesh->myKdtree.getRoot(), scn.shapes[second]->mesh->myKdtree.getRoot()));
 	
 	while (!queue.empty()) {
@@ -128,63 +122,32 @@ bool findCollision( Scene& scn, int first, int second) {
 		queue.pop();
 
 		if (checkCollision(curPair.first, curPair.second, scn.shapes[0]->makeTransScale(glm::mat4(1)), scn.shapes[1]->makeTransScale(glm::mat4(1)))) {
-			//std::cout << "in collision width:" << curPair.first->halfWidth <<" height: "<< curPair.first->halfHeight << " depth: " << curPair.first->halfDepth << std::endl;
-			//std::cout << "in collision width:" << curPair.second->halfWidth << " height: " << curPair.second->halfHeight << " depth: " << curPair.second->halfDepth << std::endl;
-			//std::cout << "------------" << std::endl;
-			//scn.draw(0, 0, true, 1, curPair.first);
-			//std::cout << " halfWidth: " << curPair.first->halfWidth << " halfHeight: " << curPair.first->halfHeight << " halfDepth: " << curPair.first->halfDepth << std::endl;
-			//std::cout << " center: " << curPair.first->center.x << " , " << curPair.first->center.y << " , " << curPair.first->center.z << std::endl;	
-			//std::cout << " 1halfWidth: " << curPair.second->halfWidth << " halfHeight: " << curPair.second->halfHeight << " halfDepth: " << curPair.second->halfDepth << std::endl;
-			//std::cout << " 1center: " << curPair.second->center.x << " , " << curPair.second->center.y << " , " << curPair.second->center.z << std::endl;
-			//scn.posititonX = 0;
-			//scn.posititonY = 0;
-
 			if (curPair.first->isLastInnerNode() && curPair.second->isLastInnerNode()) {
-				//std::cout << "got to last inner node1: " << curPair.first->halfWidth << curPair.first->halfHeight << curPair.first->halfDepth << std::endl;
-				//std::cout << "got to last inner node2: " << curPair.second->halfWidth << curPair.second->halfHeight << curPair.second->halfDepth << std::endl;
-				//std::cout << " halfWidth: " << curPair.first->halfWidth << " halfHeight: " << curPair.first->halfHeight << " halfDepth: " << curPair.first->halfDepth << std::endl;
-				//std::cout << " center: " << curPair.first->center.x << " , " << curPair.first->center.y << " , " << curPair.first->center.z << std::endl;
 				scn.posititonX = 0;
 				scn.posititonY = 0;
 				scn.draw(0, 0, true, 1, curPair.first);
 				scn.draw(0, 0, true, 0, curPair.second);
-
-				//std::cout <<" END" << std::endl;
 				return true;
 			}
 			else if (curPair.second->isLastInnerNode() && curPair.first != nullptr) {
-				//std::cout << "CASE 1" << std::endl;
 				queue.push(std::pair<Node*, Node*>(curPair.first->left, curPair.second));
 				queue.push(std::pair<Node*, Node*>(curPair.first->right, curPair.second));
 			}
 			else if (curPair.first->isLastInnerNode() && curPair.second != nullptr) {
-				//std::cout << "CASE 2" << std::endl;
-
 				queue.push(std::pair<Node*, Node*>(curPair.first, curPair.second->left));
 				queue.push(std::pair<Node*, Node*>(curPair.first, curPair.second->right));
 			}
 			else {
-				//print_queue(queue);
-
-				//std::cout << "CASE 3" << std::endl;
 				queue.push(std::pair<Node*, Node*>(curPair.first->left, curPair.second->left));
 				queue.push(std::pair<Node*, Node*>(curPair.first->right, curPair.second->left));
 				queue.push(std::pair<Node*, Node*>(curPair.first->left, curPair.second->right));
 				queue.push(std::pair<Node*, Node*>(curPair.first->right, curPair.second->right));
-				//print_queue(queue);
-
 			}
-
 		}
-
-
 	}
 
 	return true;
 }
-
-
-
 
 int main(int argc, char** argv)
 {
@@ -214,34 +177,9 @@ int main(int argc, char** argv)
 	while(!display.toClose())
 	{
 		display.Clear(0.7f, 0.7f, 0.7f, 1.0f);
-		
-		// Rotate shapes
-		/*
-		for (auto& shape : scn.shapes) {
-			shape->myRotate(0.05f, YAxis);
-		}
-		*/
-		//scn.shapes[0]->mesh->myKdtree.getRoot()->center = scn.makeTrans()*scn.shapes[0]->mesh->myKdtree.getRoot()->center;
-		//std::cout << "BEFORE center1: " << scn.shapes[1]->mesh->myKdtree.getRoot()->center.x << " , " << scn.shapes[1]->mesh->myKdtree.getRoot()->center.y << " , " << scn.shapes[1]->mesh->myKdtree.getRoot()->center.z << std::endl;
-		
-		//scn.shapes[1]->mesh->myKdtree.getRoot()->center = scn.makeTrans()*scn.shapes[1]->makeTransScale(glm::mat4(1))*firstcenter;
-		
-		//scn.shapes[1]->mesh->myKdtree.getRoot()->center.z = scn.shapes[0]->mesh->myKdtree.getRoot()->center.z;
-		//std::cout << "center: " << scn.shapes[1]->mesh->myKdtree.getRoot()->center.x<<" , "<< scn.shapes[1]->mesh->myKdtree.getRoot()->center.y << " , " << scn.shapes[1]->mesh->myKdtree.getRoot()->center.z << std::endl;
 
 		findCollision(scn,0,1);
 
-		//bool weHaveCollision = scn.shapes[1]->checkCollision(scn.shapes[1]->mesh->myKdtree.getRoot(), scn.shapes[0]->mesh->myKdtree.getRoot(), scn.shapes[0]->makeTransScale(glm::mat4(1)), scn.shapes[1]->makeTransScale(glm::mat4(1)));
-		
-		//std::cout << "AFTER center1: " << scn.shapes[1]->mesh->myKdtree.getRoot()->center.x<<" , "<< scn.shapes[1]->mesh->myKdtree.getRoot()->center.y << " , " << scn.shapes[1]->mesh->myKdtree.getRoot()->center.z << std::endl;
-		//std::cout << "center0: " << scn.shapes[0]->mesh->myKdtree.getRoot()->center.x<<" , "<< scn.shapes[0]->mesh->myKdtree.getRoot()->center.y << " , " << scn.shapes[0]->mesh->myKdtree.getRoot()->center.z << std::endl;
-
-		/*
-		if (weHaveCollision) {
-			scn.posititonX = 0;
-			scn.posititonY = 0;
-		}
-		*/
 		//move shape 1 with velocity into shape 0
 		float p1 = scn.posititonX;
 		float p2 = scn.posititonY;
